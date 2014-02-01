328865d77806beee11fb29628826bd8af1dce4e5
Eric Clark <ericbrendanclark@gmail.com>
2014-02-01 23:40:55 +0000

    added doc comment for dead_wombat describing the input and output of each function in it.

--- dead_wombat.py
+++ dead_wombat.py
@@ -1,5 +1,13 @@
 """
+holds methods to encrypt and decrypt files.
+encryption: aes_crypt(<String>password, <String>file_name)
+            returns: <String>salt(hard-coded as 'wombat'), <bytes>cryptographic key(from password), <bytes>encoded(encoded file)
 
+decryption: aes_decrypt(<bytes>key, <bytes>file_input(encrypted bytestring)  )
+            returns:<String>decrypted_string
+
+password hashing: make_hash(<String>password)
+                  returns:<bytes>pass_key(hashed password, with hardcoded salt 'wombat')
 
 
 """
@@ -10,30 +18,35 @@ import os
 from Crypto.Hash import SHA256 as SHA
 import re
 import pkcs7
+import sys
 
 def make_hash(password):
     salt = "wombat"
-    key = pbkdf2.PBKDF2(password, salt, 500, SHA).read(32)
+    pass_key = pbkdf2.PBKDF2(password, salt, 500, SHA).read(32)
+    print("key is",sys.getsizeof(pass_key),"long")
     #print(key)
-    return salt
+    return pass_key
 
 def aes_crypt(password, file_name):
     salt = "wombat"
+
     padder = pkcs7.PKCS7Encoder()
-    key = pbkdf2.PBKDF2(password, salt, 1000, SHA).read(32)
-    cipher = AES.new(key)
+    crypt_key = pbkdf2.PBKDF2(password, salt, 1000, SHA).read(32)
+    print("key is ",sys.getsizeof(crypt_key),"long")
+    cipher = AES.new(crypt_key)
     file_input = open(file_name)
     file_input = file_input.read()
-    print("input file is{\n ",file_input,"\n}")
-    print("padding needed for file is: ", len(file_input) % 16)
+    print("input file is{\n",file_input,"}")
+    #print("padding needed for file is: ", len(file_input) % 16)
     pad_str = padder.encode(file_input)
     encoded = cipher.encrypt(pad_str)
-    print(pad_str)
+    #print(pad_str)
 
-    return salt, key, encoded
+    return salt, crypt_key, encoded
 
 def aes_decrypt(key, file_input):
     cipher = AES.new(key)
     decoded = cipher.decrypt(file_input).decode()
     padder = pkcs7.PKCS7Encoder()
-    return padder.decode(decoded)
+    decrypted_string = padder.decode(decoded)
+    return decrypted_string
