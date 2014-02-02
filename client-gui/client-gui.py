c8f3716fe766a598b5f1677edcd4c566ef2805b6
donjuma <Omar.Juma@wwu.edu>
2014-02-01 16:13:04 -0800

    Linked the two QT widgets

--- client-gui/client-gui.py
+++ client-gui/client-gui.py
@@ -1,21 +1,44 @@
 from PyQt4 import QtGui, QtCore
 import sys, signal
-from test import Ui_Form
+from resources.auth import Ui_authForm
+from resources.dirs import Ui_dirForm
 
-class wombatClient(QtGui.QWidget):
-	def __init__(self):
-		QtGui.QWidget.__init__(self)
 
-		self.ui = Ui_Form()
-		self.ui.setupUi(self)
+class authClient(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
 
+        self.auth = Ui_authForm()
+        self.auth.setupUi(self)
+
+        self.auth.loginButton.clicked.connect(self.authenticate)
+        #self.auth.lineEdit.returnPressed.connect(self.send)
+        #self.auth.connectButton.clicked.connect(self.connect)
+        #self.auth.disconnectButton.clicked.connect(self.disconnect)
+
+    def authenticate(self):
+
+    def showDirWidget(self):
+        window.hide()
+        global window
+        window = dirSelector()
+        window.show()
+
+
+class dirSelector(QtGui.QWidget):
+    def __init__(self):
+       QtGui.QWidget.__init__(self)
+
+       self.dirSelection = Ui_dirForm()
+       self.dirSelection.setupUi(self)
 
 def main():
-	app = QtGui.QApplication(sys.argv)
-	window = wombatClient()
-	window.show()
-	signal.signal(signal.SIGINT, signal.SIG_DFL)
-	sys.exit(app.exec_())
+    global window
+    app = QtGui.QApplication(sys.argv)
+    window = authClient()
+    window.show()
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
 	main()
