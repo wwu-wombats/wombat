54062a9f74ebc976d1ac7111dda71f2a48e33629
Eric Clark <ericbrendanclark@gmail.com>
2014-02-02 01:38:25 +0000

    encrypt and decrypt are consistent within my programs. cannot communicate with the web encryption/decryption

--- decrypt_file.py
+++ decrypt_file.py
@@ -1,13 +1,13 @@
 import dead_wombat
 crypt_key = dead_wombat.make_aes_key("secretkey")
 
-encrypted_file = open('server/files/admin/test.txt', 'r')
-file_input = encrypted_file.read().encode()
+encrypted_file = open('encrypted_output', 'rb')
+file_input = encrypted_file.read()
 print("encrypted_file is :\n",file_input)
 print("size of file is:",len(file_input))
 
 decoded_file = dead_wombat.aes_decrypt_key(crypt_key, file_input)
-print("decoded file is {\n",decoded_file,"\n}")
+#print("decoded file is {\n",decoded_file.encode('UTF-32'),"\n}")
 file_save = open('test_output.txt', 'w')
 file_save.write(decoded_file)
 file_save.close()
